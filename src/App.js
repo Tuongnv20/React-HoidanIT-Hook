68817f42da7e0259fc77f64ba605ae3baf187ba1
Tuong <tuong.wall123@gmail.com>
2023-06-11 21:08:52 +0700

    State and Use State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [name, setName] = useState('Wall');
+  const [name1, setName1] = useState('');
+
+  const handleEventClick = () => {
+    setName(name1);
+  };
+
+  const handleOnchangeInput = (event) => {
+    setName1(event.target.value)
+  }
+
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <p>Hello world with {name}</p>
+        <input type='text' value={name1} onChange={(event) => handleOnchangeInput(event)} />
+        <button type='button' onClick={handleEventClick}>Click me</button>
       </header>
     </div>
   );
